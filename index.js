85eff274b383069b88e32024837804318f9a9c78
leandrobacelar <leandrobacelar710@gmail.com>
2024-12-29 18:29:42 -0300

    proyect-completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("working");
+const button = document.querySelector(".action-button");
+const titleCard = document.querySelector(".card-title");
+
+button.addEventListener("click", handleClick);
+
+function handleClick() {
+  titleCard.style.color = randomColor();
+}
+
+function randomColor() {
+  const color = Math.floor(Math.random() * 16777215).toString(16);
+  return "#" + color;
+}
